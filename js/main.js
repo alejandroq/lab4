49c711fc3f3f4e6c6e59d2c9fc55855fda13d65a
Alejandro Quesada <alejandroq@users.noreply.github.com>
2016-02-24 17:47:39 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,35 +1,40 @@
 
 //AJAX REQUEST
-$('#employeeSubmit').submit(function() {
+$(function() {
+  $('.errorMsg').hide();
+  
+  $('#employeeSubmit').submit(function() {
 
-  var firstName, lastName, MI; 
+    var firstName, lastName, MI; 
 
-  firstName = $('#firstName').val();
-  lastName = $('#lastName').val();
-  MI = $('#MI').val();
+    firstName = $('#firstName').val();
+    lastName = $('#lastName').val();
+    MI = $('#MI').val();
 
-  var data = new Object();
-  data.firstName = firstName;
-  data.lastName = lastName;
-  data.MI = MI; 
+    var data = new Object();
+    data.firstName = firstName;
+    data.lastName = lastName;
+    data.MI = MI; 
 
-  var options = new Object();
-  options.data = data;
-  options.dataType = 'text';
-  options.type = 'GET';
-  options.success = function(response) {
-    //if AJAX Request is a success
+    var options = new Object();
+    options.data = data;
+    options.dataType = 'text';
+    options.type = 'get';
+    options.success = function(response) {
+      //if AJAX Request is a success
 
-    if (response == 'CORRECT') {
-      $('#info').text('Thank you for your submission!');
-    }
-    else 
-    {
-      $('#info').text('Something appears to be wrong... Try again?')
-    } 
-  } // END OF AJAX SUCCESS
-  options.url = 'employeeSubmit.php';
+      if (response == 'CORRECT') {
+        $('#info').text('Thank you for your submission!');
+      }
+      else 
+      {
+        $('#info').text('Something appears to be wrong... Try again?')
+      } 
 
-  //PERFORM REQUEST
-  $.ajax(options);
-});
+    }; // END OF AJAX SUCCESS
+    options.url = 'employeeSubmit.php';
+
+    //PERFORM REQUEST
+    $.ajax(options);
+  }); //END OF FORM SUBMISSION
+}); //END OF DOCUMENT READY
