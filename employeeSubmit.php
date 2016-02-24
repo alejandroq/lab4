e3796b294fb24088b2aed4c0c04c21e98201cfd4
Alejandro Quesada <alejandroq@users.noreply.github.com>
2016-02-24 18:04:53 -0500

    Update employeeSubmit.php

--- employeeSubmit.php
+++ employeeSubmit.php
@@ -4,7 +4,7 @@
 //This script expects to recieve all valuations required for an Employee Submission. 
 //The script returns a string indicating results. 
 
-if (isset($_GET['firstName']), isset($_GET['lastName']), isset($_GET['MI'])) {
+if (isset($_GET['firstName'] ,$_GET['lastName'],$_GET['MI'])) {
 
     echo 'CORRECT';
 }
