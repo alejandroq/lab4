e098cf8efb7b825dd8fd8b0aed9d795de99cb9c1
Alejandro Quesada <alejandroq@users.noreply.github.com>
2016-02-25 11:10:26 -0500

    Added files via upload

---
+++ functions.php
@@ -0,0 +1,43 @@
+<?php
+
+function printCountries() 
+{
+	require 'connection.php';
+
+    $sql = 'SELECT * FROM country ORDER BY CountryAbbreviation DESC';
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) 
+    {
+        while ($row = $result->fetch_assoc()) 
+        {
+            $CountryAbbreviation = $row['CountryAbbreviation'];
+            $CountryName = $row['CountryName'];
+
+            echo '
+            	<option value="' . $CountryAbbreviation . '">' . $CountryName . '</option>';
+        }
+    }
+}
+
+function printStates()
+{
+	require 'connection.php';
+
+    $sql = 'SELECT * FROM state ORDER BY StateAbbreviation DESC';
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) 
+    {
+        while ($row = $result->fetch_assoc()) 
+        {
+            $StateAbbreviation = $row['StateAbbreviation'];
+            $StateName = $row['StateName'];
+
+            echo '
+            	<option value="' . $StateAbbreviation . '">' . $StateName . '</option>';
+        }
+    }
+}
+
+?>
