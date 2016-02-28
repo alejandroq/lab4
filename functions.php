d6b4d6e422ce33bc5c35ac57f19a11040bc9300b
Alejandro Quesada <alejandroq@users.noreply.github.com>
2016-02-28 00:28:32 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,12 @@
 <?php
 
+//Checking if "Update Dashboard" button was pressed. 
+// if(isset($_POST['function'])) { 
+//     if($_POST['function'] == 'time') {
+//     	echo printDashboard();
+//     }
+// }
+
 function printCountries() 
 {
 	require 'connection.php';
@@ -38,6 +45,104 @@ function printStates()
             	<option value="' . $StateAbbreviation . '">' . $StateName . '</option>';
         }
     }
+}
+
+function printDashboard()
+{
+	require 'connection.php';
+
+	echo '<table>';
+
+	$sql = 'SELECT CONCAT(ifNull(lastName, "") , ", " , ifNull(firstName,"")) Name, ifNull(Accident.AccidentLocation, "")AccidentLocation FROM employee 
+		RIGHT JOIN accident ON employee.EmployeeID = accident.EmployeeID';
+
+    $result = $conn->query($sql);
+
+
+    if ($result->num_rows > 0) 
+	{
+		while ($row = $result->fetch_assoc())
+		{
+			$name = $row['Name'];
+			$location = $row['Accident.AccidentLocation'];
+
+			echo 
+			' <tr> 
+				<td> ' . $name . '</td> 
+				<td> ' . $location . '</td> 
+			 </tr>';
+		}
+	}
+
+	$sql = 'SELECT SUM(salary) Salary FROM Employee';
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0)
+	{
+		while ($row = $result->fetch_assoc())
+		{
+			$salary = $row['Salary'];
+			echo 
+			'
+				<tr>
+					<td> ' . $salary . ' </td>
+				</tr>
+			';
+		}
+	}
+
+	$sql = 'SELECT * FROM Truck';
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0)
+	{
+		while ($row = $result->fetch_assoc())
+		{
+			$vinNumber = $row['VinNumber'];
+			$make = $row['Make'];
+			$model = $row['Model'];
+			$priceAcquired = $row['PriceAcquired'];
+			$licenseDate = $row['LicenseDate'];
+
+			echo 
+			'
+				<tr> 
+					<td> ' . $vinNumber . ' </td>
+					<td> ' . $make . ' </td>
+					<td> ' . $model . ' </td>
+					<td> ' . $priceAcquired . ' </td>
+					<td> ' . $licenseDate . ' </td>
+				</tr>
+			';
+		}
+	}
+
+	echo '</table>';
+}
+
+function printEmployees()
+{
+	require 'connection.php';
+
+	$sql = 'SELECT EmployeeID, CONCAT(lastName , ", " , firstName) Name FROM Employee';
+	$result = $conn->query($sql);
+
+	print_r($result);
+
+	if ($result->num_rows > 0)
+	{
+		while ($row = $result->fetch_assoc())
+		{
+			$employeeID = $row['EmployeeID'];
+			$name = $row['Name'];
+
+			echo 
+			'
+				<option value=' . $employeeID . '>' . $name . '<option>
+			';
+		}
+	}
+
 }
 
 ?>
